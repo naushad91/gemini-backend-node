c678f76e8dea60a30e9c24f9ecd19652949ad43d
Naushad Khan <naushadkhank426@gmail.com>
2025-09-14 20:24:05 +0530

    feat(db): updated userschema

---
+++ prisma/migrations/20250914145246_updated_user_fields_inschema/migration.sql
@@ -0,0 +1,13 @@
+/*
+  Warnings:
+
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+
+*/
+-- AlterTable
+ALTER TABLE "public"."User" ADD COLUMN     "email" TEXT,
+ADD COLUMN     "name" TEXT,
+ADD COLUMN     "password_hash" TEXT;
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "public"."User"("email");
