f2fbe020e90f0304980273e98e21893ebf0920ac
Naushad Khan <naushadkhank426@gmail.com>
2025-09-14 14:49:34 +0530

    feat(db): connected Prisma to Neon, prisma client generated and tested /endpont

--- src/db.js
+++ src/db.js
@@ -1,4 +1,3 @@
-// src/db.js
 const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 module.exports = prisma;
--- src/index.js
+++ src/index.js
@@ -1,15 +1,15 @@
-// src/index.js
 const express = require("express");
 const prisma = require("./db");
 
 const app = express();
 app.use(express.json());
 
+// test endpoint
 app.get("/", async (req, res) => {
   const users = await prisma.user.findMany();
   res.json({ msg: "Gemini backend running...", users });
 });
 
 app.listen(8000, () => {
-  console.log("✅ Server running on http://localhost:8000");
+  console.log("Server running on http://localhost:8000");
 });
