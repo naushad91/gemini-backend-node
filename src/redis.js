921708543b5aa7944956d6a0356e2aaa545f38ab
Naushad Khan <naushadkhank426@gmail.com>
2025-09-20 16:06:58 +0530

    redis  updated for deplyment using upstash redis cloud

--- src/redis.js
+++ src/redis.js
@@ -1,6 +1,26 @@
+// const Redis = require("ioredis");
+
+// const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+
+// module.exports = redis;
+
+
 const Redis = require("ioredis");
 
-const redis = new Redis(process.env.REDIS_URL || "redis://localhost:6379");
+let redis;
+
+if (process.env.REDIS_URL) {
+  // Upstash (cloud)
+  redis = new Redis(process.env.REDIS_URL, {
+    tls: {}, // Upstash requires TLS
+  });
+} else {
+  // Local dev (docker redis)
+  redis = new Redis({
+    host: process.env.REDIS_HOST || "127.0.0.1",
+    port: process.env.REDIS_PORT || 6379,
+    password: process.env.REDIS_PASSWORD || undefined,
+  });
+}
 
 module.exports = redis;
-
